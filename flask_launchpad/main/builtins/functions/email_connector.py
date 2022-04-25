28ac074514df6c0b80847ea276eebac6605c9785
David Carmichael <carmichaelits@gmail.com>
2022-04-25 16:58:04 +0100

    Updated email connector

--- flask_launchpad/main/builtins/functions/email_connector.py
+++ flask_launchpad/main/builtins/functions/email_connector.py
@@ -11,15 +11,38 @@ from .utilities import get_file_extension
 app_config = read_app_config(section="smtp")
 
 
-def send_email(subject: str, email_to: str, email_body: str, attached_files: list = None) -> list:
+def send_email(
+        subject: str,
+        email_to: str,
+        email_body: str,
+        attached_files: list = None,
+        from_name: str = None,
+        send_from: str = None,
+        reply_to: str = None,
+        username: str = None,
+        password: str = None,
+        server: str = None,
+        port: int = None,
+) -> list:
     """
     Sends a plain HTML email.
-    :param subject:
-    :param email_to:
-    :param email_body:
-    :param attached_files:
-    :return list[bool, status]:
     """
+
+    if from_name is None:
+        from_name = app_config["from_name"]
+    if send_from is None:
+        send_from = app_config["send_from"]
+    if reply_to is None:
+        reply_to = app_config["reply_to"]
+    if username is None:
+        username = app_config["username"]
+    if password is None:
+        password = app_config["password"]
+    if server is None:
+        server = app_config["server"]
+    if port is None:
+        port = app_config["port"]
+
     html_msg = MIMEText(email_body)
     html_msg.set_type('text/html')
     html_msg.set_param('charset', 'UTF-8')
@@ -28,9 +51,9 @@ def send_email(subject: str, email_to: str, email_body: str, attached_files: lis
     msg.set_type('multipart/alternative')
     msg['Subject'] = subject
     msg['To'] = email_to
-    msg['From'] = f'"{app_config["from_name"]}"' + f'<{app_config["send_from"]}>'
-    msg['Reply-To'] = app_config["reply_to"]
-    msg['Original-Sender'] = app_config["username"]
+    msg['From'] = f'"{from_name}"' + f'<{send_from}>'
+    msg['Reply-To'] = reply_to
+    msg['Original-Sender'] = username
     msg.attach(html_msg)
 
     for attached_file in attached_files or []:
@@ -41,26 +64,39 @@ def send_email(subject: str, email_to: str, email_body: str, attached_files: lis
         msg.attach(contents)
 
     try:
-        with SMTP(app_config["server"], app_config["port"]) as connection:
+        with SMTP(server, port) as connection:
             connection.starttls()
-            connection.login(app_config["username"], app_config["password"])
-            connection.sendmail(app_config["send_from"], email_to, msg.as_string())
+            connection.login(username, password)
+            connection.sendmail(send_from, email_to, msg.as_string())
     except SMTPException as error:
         return [False, "AUTHENTICATION OR CONNECTION ISSUE", error]
 
     return [True, "EMAIL SENT", None]
 
 
-def test_email_server_connection() -> bool:
+def test_email_server_connection(
+        username: str = None,
+        password: str = None,
+        server: str = None,
+        port: str = None,
+) -> bool:
     """
     Used to test the settings of the smtp settings.
-    :return list[bool, status]:
     """
+    if username is None:
+        username = app_config["username"]
+    if password is None:
+        password = app_config["password"]
+    if server is None:
+        server = app_config["server"]
+    if port is None:
+        port = app_config["port"]
+
     try:
         ssl_context = create_default_context()
-        with SMTP(app_config["server"], app_config["port"]) as connection:
+        with SMTP(server, port) as connection:
             connection.starttls(context=ssl_context)
-            connection.login(app_config["username"], app_config["password"])
+            connection.login(username, password)
     except SMTPException:
         return False
 
