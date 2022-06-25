24a0b3725b1b389ab23c7a7fcdd55808cbb37821
David Carmichael <carmichaelits@gmail.com>
2022-06-25 14:42:04 +0100

    Cleaned up Flask_launchpad init file. Put version 0.6.7 in old, version 0.6.8 released.

--- main/_flask_launchpad/src/flask_launchpad/__init__.py
+++ main/_flask_launchpad/src/flask_launchpad/__init__.py
@@ -4,8 +4,7 @@ from .app import FLStructure as FLStructure
 from .app import model_module as model_module
 from .app import model_class as model_class
 from .app import sql_do as sql_do
-from .auth import Auth as Auth
 from .auth import WordGen as WordGen
 from .security import Security as Security
 
-version = "0.6.7"
+version = "0.6.8"
