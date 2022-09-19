6020306c20a970072cec9ade5dc729aea7ee5b8c
David Carmichael <carmichaelits@gmail.com>
2022-09-19 14:57:40 +0100

    removal of unused package and docs.md file

--- src/flask_bigapp/__init__.py
+++ src/flask_bigapp/__init__.py
@@ -4,4 +4,4 @@ from .BigApp import BigApp as BigApp
 from .Blueprint import BigAppBlueprint as Blueprint
 from .Security import BigAppSecurity as Security
 
-__all__ = ["Auth", "PasswordGeneration", "BigApp", "Blueprint", "Security", "Types"]
+__all__ = ["Auth", "PasswordGeneration", "BigApp", "Blueprint", "Security"]
