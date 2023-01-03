439e0aafda3a10c96ba41dd64b1f1e85ff2efd9f
david <carmichaelits@gmail.com>
2023-01-03 02:21:48 +0000

    Added db creation back in

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bigapp import BigApp
 from flask_sqlalchemy import SQLAlchemy
+from flask_bigapp import Auth
 
 bigapp = BigApp()
 db = SQLAlchemy()
@@ -25,42 +26,42 @@ def create_app():
     def after_request(response):
         return response
 
-    #
-    # with main.app_context():
-    #     """
-    #     The following creates all tables from the model files and populates the database
-    #     with test data.
-    #     """
-    #     db.create_all()
-    #
-    #     m_example_user = bigapp.model_class("ExampleUser")
-    #     m_example_table = bigapp.model_class("ExampleTable")
-    #
-    #     if not m_example_user.get_by_id(1):
-    #         salt = Auth.generate_salt()
-    #         gen_password = Auth.generate_password("animals")
-    #         password = Auth.sha_password(gen_password, salt)
-    #
-    #         new_example_user = m_example_user(
-    #             username="David",
-    #             password=password,
-    #             salt=salt,
-    #             private_key=Auth.generate_private_key(salt),
-    #             disabled=False
-    #         )
-    #         db.session.add(new_example_user)
-    #         db.session.flush()
-    #         new_example_user_rel = m_example_table(
-    #             user_id=new_example_user.user_id,
-    #             thing=gen_password
-    #         )
-    #         db.session.add(new_example_user_rel)
-    #         db.session.commit()
+
+    with main.app_context():
+        """
+        The following creates all tables from the model files and populates the database
+        with test data.
+        """
+        db.create_all()
+
+        m_example_user = bigapp.model_class("ExampleUser")
+        m_example_table = bigapp.model_class("ExampleTable")
+
+        if not m_example_user.get_by_id(1):
+            salt = Auth.generate_salt()
+            gen_password = Auth.generate_password("animals")
+            password = Auth.sha_password(gen_password, salt)
+
+            new_example_user = m_example_user(
+                username="David",
+                password=password,
+                salt=salt,
+                private_key=Auth.generate_private_key(salt),
+                disabled=False
+            )
+            db.session.add(new_example_user)
+            db.session.flush()
+            new_example_user_rel = m_example_table(
+                user_id=new_example_user.user_id,
+                thing=gen_password
+            )
+            db.session.add(new_example_user_rel)
+            db.session.commit()
 
     """
     This prints all the available routes in the app
     """
-    # for _ in main.url_map.iter_rules():
-    #     print(_)
+    for _ in main.url_map.iter_rules():
+        print(_)
 
     return main
