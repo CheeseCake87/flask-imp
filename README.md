d7ad1912f164907a0ebbafa6faceeab0b4085d8c
david <carmichaelits@gmail.com>
2023-01-05 12:56:43 +0000

    Changed version structure

--- README.md
+++ README.md
@@ -2,18 +2,13 @@
 
 # Flask-BigApp
 
-`pip install flask-bigapp`
+`pip install Flask-BigApp`
 
 ![Tests](https://github.com/CheeseCake87/Flask-BigApp/actions/workflows/tests.yml/badge.svg)
-
-`NOTE:` This version; Being 1.5.* and above includes some breaking changes from anything on version 1.4.* and below. 
-There will be no more breaking changes from this version. Each update will enact deprecation.
 
 **What is Flask-BigApp?**
 
 Flask-BigApp's main purpose is to help simplify the importing of blueprints, routes and models.
 It has a few extra features built in to help with theming, securing pages and password authentication.
-
-## Wiki
 
 [Go to Wiki](https://github.com/CheeseCake87/Flask-BigApp/wiki)
--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 import pathlib
-
 from setuptools import setup, find_packages
 
-
-def read(file_name):
-    return pathlib.Path(pathlib.Path.cwd() / file_name).read_text()
-
+readme = pathlib.Path(pathlib.Path.cwd() / "README.md").read_text()
 
 setup(
-    name='flask-bigapp',
-    version='1.6.3',
+    name='Flask-BigApp',
+    version=f'2023.1.0',
     url='https://github.com/CheeseCake87/Flask-BigApp',
     license='GNU General Public License v2 or later (GPLv2+)',
     author='David Carmichael',
     author_email='carmichaelits@gmail.com',
     description='A Flask auto importer that allows your Flask apps to grow big.',
-    long_description=read('README.md'),
+    long_description=f'{readme}',
     long_description_content_type='text/markdown',
     platforms='any',
     classifiers=[
