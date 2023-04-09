3d8f2e99891906ba31014f77b6c585968d65b437
David Carmichael <carmichaelits@gmail.com>
2023-04-09 20:58:05 +0100

    New decorator functions for checking if a user is logged in, or a user has the correct permissions.

--- src/flask_bigapp/security.py
+++ src/flask_bigapp/security.py
@@ -6,6 +6,128 @@ from flask import session
 from flask import url_for
 
 
+def login_check(
+        bool_session_key: str,
+        endpoint_redirect: str,
+        redirect_on_value: bool = False,
+        endpoint_kwargs: dict = None,
+        message: str = None,
+        message_category: str = "message"
+):
+    """
+    A decorator that checks if the specified session key exists, redirects to the specified endpoint if it does not
+    exist or match the specified value.
+
+    Example of a route that requires a user to be logged in:
+
+    @bp.route("/admin", methods=["GET"])
+    @login_check('logged_in', 'blueprint.login_page', message="Login needed")
+    def admin_page():
+        ...
+
+    Example of a route that if the user is already logged in, redirects to the specified endpoint:
+
+    @bp.route("/login-page", methods=["GET"])
+    @login_check('logged_in', 'blueprint.admin_page', redirect_on_value=True, message="Already logged in")
+    def login_page():
+        ...
+
+    :param bool_session_key: The session key to check for.
+    :param endpoint_redirect: The endpoint to redirect to
+                              if the session key does not exist or
+                              match the specified value.
+    :param redirect_on_value: The boolean value to check the session key against.
+    :param endpoint_kwargs: A dictionary of keyword arguments to pass to the redirect endpoint.
+    :param message: If a message is specified, a flash message is shown.
+    :param message_category: The category of the flash message.
+    """
+
+    def login_check_wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            if bool_session_key not in session:
+                if message:
+                    flash(message, message_category)
+                if endpoint_kwargs is None:
+                    return redirect(url_for(endpoint_redirect))
+                return redirect(url_for(endpoint_redirect, **endpoint_kwargs))
+
+            if session[bool_session_key] == redirect_on_value:
+                if message:
+                    flash(message, message_category)
+                if endpoint_kwargs is None:
+                    return redirect(url_for(endpoint_redirect))
+                return redirect(url_for(endpoint_redirect, **endpoint_kwargs))
+
+            return func(*args, **kwargs)
+
+        return inner
+
+    return login_check_wrapper
+
+
+def permission_check(
+        iter_session_key: str,
+        endpoint_redirect: str,
+        redirect_if_no_match: list = None,
+        endpoint_kwargs: dict = None,
+        message: str = None,
+        message_category: str = "message"
+):
+    """
+    A decorator that checks if the specified session key exists, redirects to the specified endpoint if
+    the session key does not exist or if the session key does not contain the specified values.
+
+    Example:
+
+    @bp.route("/admin-page", methods=["GET"])
+    @login_check('logged_in', 'blueprint.login_page') <- can be mixed with login_check
+    @permission_check('permissions', 'www.index', redirect_if_no_match=['admin'],
+                      message="You don't have the correct permissions to view this page.")
+    def admin_page():
+        ...
+
+
+    IF redirect_if_no_match is None, the default value of ['admin'] is set.
+
+    :param iter_session_key: The session key to check for.
+    :param endpoint_redirect: The endpoint to redirect to if the
+                                session key does not exist or does not contain the
+                                specified values.
+    :param redirect_if_no_match: A list of values that the session key must contain.
+    :param endpoint_kwargs: A dictionary of keyword arguments to pass to the redirect endpoint.
+    :param message: If a message is specified, a flash message is shown.
+    :param message_category: The category of the flash message.
+    """
+
+    if redirect_if_no_match is None:
+        redirect_if_no_match = ['admin']
+
+    def permission_check_wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            if iter_session_key not in session:
+                if message:
+                    flash(message, message_category)
+                if endpoint_kwargs is None:
+                    return redirect(url_for(endpoint_redirect))
+                return redirect(url_for(endpoint_redirect, **endpoint_kwargs))
+
+            for item in redirect_if_no_match:
+                if item not in session[iter_session_key]:
+                    if message:
+                        flash(message, message_category)
+                    if endpoint_kwargs is None:
+                        return redirect(url_for(endpoint_redirect))
+                    return redirect(url_for(endpoint_redirect, **endpoint_kwargs))
+
+            return func(*args, **kwargs)
+
+        return inner
+
+    return permission_check_wrapper
+
+
 class BigAppSecurity:
     """
     A class that provides route function decorators to check for session variables and their values.
