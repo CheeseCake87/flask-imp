45210b212a52f62074bb5cbe1712db8ff1179119
David Carmichael <carmichaelits@gmail.com>
2023-04-09 20:57:28 +0100

    login check example added.

--- app/www/routes/index.py
+++ app/www/routes/index.py
@@ -1,10 +1,19 @@
 from flask import render_template
+from flask_bigapp.security import login_check
 
 from .. import bp
 
 
 @bp.route("/", methods=["GET"])
 def index():
+    return render_template(
+        bp.tmpl("index.html")
+    )
+
+
+@bp.route("/login-test", methods=["GET"])
+@login_check('logged_in', 'www.index', message="Test message")
+def login_test():
     return render_template(
         bp.tmpl("index.html")
     )
