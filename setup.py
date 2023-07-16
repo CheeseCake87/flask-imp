a85658fd29a2a642b48ab7c8ad3b5d57b01ad68b
David Carmichael <carmichaelits@gmail.com>
2023-07-16 21:58:04 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -1,8 +1,65 @@
-import pathlib
-
 from setuptools import setup, find_packages
 
-readme = pathlib.Path(pathlib.Path.cwd() / "PYPI-README.md").read_text()
+readme = """
+![](https://raw.githubusercontent.com/CheeseCake87/Flask-BigApp/master/_assets/Flask-BigApp-v2-Small.png)
+
+# Flask-BigApp
+
+![Tests](https://github.com/CheeseCake87/Flask-BigApp/actions/workflows/tests.yml/badge.svg)
+
+## What is Flask-BigApp?
+
+Flask-BigApp's main purpose is to help simplify the importing of blueprints, resources and models.
+It has a few extra features built in to help with securing pages and password authentication.
+
+## Getting Started
+
+### Setup.
+
+Create a new project folder and navigate to it.
+
+```text
+# Linux
+cd /path/to/project-folder
+
+# Windows
+cd C:\path\\to\project-folder
+```
+
+### Create a virtual environment and activate it.
+
+**Linux / MacOS**
+
+```bash
+python3 -m venv venv
+```
+
+```bash
+source venv/bin/activate
+```
+
+**Windows**
+
+```bash
+python -m venv venv
+```
+
+```bash
+.\\venv\Scripts\\activate
+```
+
+### Install Flask-BigApp
+
+```bash
+pip install flask-bigapp
+```
+
+### Create a new project.
+
+```bash
+flask-bigapp init
+```
+"""
 
 setup(
     name='Flask-BigApp',
