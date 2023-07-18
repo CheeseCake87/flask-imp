3231688bead49a2fc09b125f341c72eda8b9b9b5
David Carmichael <carmichaelits@gmail.com>
2023-07-18 23:28:21 +0100

    Version updated

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ flask-bigapp init
 
 setup(
     name='Flask-BigApp',
-    version='2.0.3',
+    version='2.0.4',
     url='https://github.com/CheeseCake87/Flask-BigApp',
     license='GNU Lesser General Public License v2.1',
     author='David Carmichael',
