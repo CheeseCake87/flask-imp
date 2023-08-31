3b480a2f95734615c074b1a1dec512d20ec24d90
David Carmichael <carmichaelits@gmail.com>
2023-08-31 19:46:53 +0100

    Removed unsupported operand, updated version number.

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ flask-bigapp init
 
 setup(
     name='Flask-BigApp',
-    version='2.2.2',
+    version='2.3.0',
     url='https://github.com/CheeseCake87/Flask-BigApp',
     license='GNU Lesser General Public License v2.1',
     author='David Carmichael',
