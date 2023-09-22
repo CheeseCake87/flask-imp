7bc62d81ec1835aeadacd97d3bdc06396db01877
David Carmichael <carmichaelits@gmail.com>
2023-09-22 15:34:35 +0100

    Doc string overhaul.

--- src/flask_imp/security.py
+++ src/flask_imp/security.py
@@ -16,6 +16,9 @@ def _check_against_values_allowed(
         session_value: t.Union[list, str, int, bool],
         values_allowed: t.Union[t.List[t.Union[str, int, bool]], str, int, bool],
 ) -> bool:
+    """
+    Checks if the session value matches the values allowed. Used by login_check and permission_check.
+    """
     if isinstance(values_allowed, list):
         if isinstance(session_value, list):
             for value in session_value:
@@ -45,19 +48,35 @@ def login_check(
     """
     A decorator that checks if the specified session key exists and contains the specified value.
 
-    Example of a route that requires a user to be logged in:
+    :raw-html:`<br />`
 
-    @bp.route("/admin", methods=["GET"])
-    @login_check('logged_in', True, 'blueprint.login_page', message="Login needed")
-    def admin_page():
-        ...
+    **Example of a route that requires a user to be logged in:**
 
-    Example of a route that if the user is already logged in, redirects to the specified endpoint:
+    :raw-html:`<br />`
 
-    @bp.route("/login-page", methods=["GET"])
-    @login_check('logged_in', True, 'blueprint.admin_page', message="Already logged in")
-    def login_page():
-        ...
+    .. code-block::
+
+        @bp.route("/admin", methods=["GET"])
+        @login_check('logged_in', True, fail_endpoint='blueprint.login_page', message="Login needed")
+        def admin_page():
+            ...
+
+    :raw-html:`<br />`
+
+    **Example of a route that if the user is already logged in, redirects to the specified endpoint:**
+
+    :raw-html:`<br />`
+
+    .. code-block::
+
+        @bp.route("/login-page", methods=["GET"])
+        @login_check('logged_in', True, pass_endpoint='blueprint.admin_page', message="Already logged in")
+        def login_page():
+            ...
+
+    :raw-html:`<br />`
+
+    -----
 
     :param session_key: The session key to check for.
     :param values_allowed: A list of or singular value(s) that the session key must contain.
@@ -68,6 +87,7 @@ def login_check(
     :param endpoint_kwargs: A dictionary of keyword arguments to pass to the redirect endpoint.
     :param message: If a message is specified, a flash message is shown.
     :param message_category: The category of the flash message.
+    :return: The decorated function, or abort(403).
     """
 
     def login_check_wrapper(func):
@@ -135,13 +155,23 @@ def permission_check(
     """
     A decorator that checks if the specified session key exists and its value(s) match the specified value(s).
 
-    Example:
+    :raw-html:`<br />`
 
-    @bp.route("/admin-page", methods=["GET"])
-    @login_check('logged_in', 'blueprint.login_page') <- can be mixed with login_check
-    @permission_check('permissions', ['admin'], 'www.index', message="Failed message", message_category="error")
-    def admin_page():
-        ...
+    **Example:**
+
+    :raw-html:`<br />`
+
+    .. code-block::
+
+        @bp.route("/admin-page", methods=["GET"])
+        @login_check('logged_in', True, 'blueprint.login_page')  # can be mixed with login_check
+        @permission_check('permissions', ['admin'], fail_endpoint='www.index', message="Failed message")
+        def admin_page():
+            ...
+
+    :raw-html:`<br />`
+
+    -----
 
     :param session_key: The session key to check for.
     :param values_allowed: A list of or singular value(s) that the session key must contain.
@@ -151,6 +181,7 @@ def permission_check(
     :param endpoint_kwargs: A dictionary of keyword arguments to pass to the redirect endpoint.
     :param message: If a message is specified, a flash message is shown.
     :param message_category: The category of the flash message.
+    :return: The decorated function, or abort(403).
     """
 
     def permission_check_wrapper(func):
@@ -192,29 +223,42 @@ def api_login_check(
         fail_json: t.Optional[t.Dict[str, t.Any]] = None
 ):
     """
-    A decorator that is used to secure API routes that return a JSON response.
+    A decorator that is used to secure API routes that return JSON responses.
 
-    Example of a route that requires a user to be logged in:
+    :raw-html:`<br />`
 
-    @bp.route("/api/resource", methods=["GET"])
-    @api_login_check('logged_in', True)
-    def api_page():
-        ...
+    **Example of a route that requires a user to be logged in:**
 
+    :raw-html:`<br />`
 
-    You can also supply your own failed return JSON:
+    .. code-block::
 
-    @bp.route("/api/resource", methods=["GET"])
-    @api_login_check('logged_in', True, fail_json={"error": "You are not logged in."})
-    def api_page():
-        ...
+        @bp.route("/api/resource", methods=["GET"])
+        @api_login_check('logged_in', True)
+        def api_page():
+            ...
 
+    :raw-html:`<br />`
 
-    Default json_to_return is {"error": "You are not logged in."}
+    **You can also supply your own failed return JSON:**
+
+    :raw-html:`<br />`
+
+    .. code-block::
+
+        @bp.route("/api/resource", methods=["GET"])
+        @api_login_check('logged_in', True, fail_json={"error": "You are not logged in."})
+        def api_page():
+            ...
+
+    :raw-html:`<br />`
+
+    -----
 
     :param session_key: The session key to check for.
     :param values_allowed: A list of or singular value(s) that the session key must contain.
-    :param fail_json: JSON that is returned on failure.
+    :param fail_json: JSON that is returned on failure. {"error": "You are not logged in."} by default.
+    :return: The decorated function, or a JSON response.
     """
 
     def api_login_check_wrapper(func):
@@ -233,10 +277,45 @@ def api_login_check(
     return api_login_check_wrapper
 
 
-def include_csrf(session_key: str = "csrf", form_key: str = "csrf"):
+def include_csrf(session_key: str = "csrf", form_key: str = "csrf", abort_code: int = 401):
     """
     A decorator that handles CSRF protection.
+
+    :raw-html:`<br />`
+
+    On a **GET** request, a CSRF token is generated and stored in the session key
+    specified by the session_key parameter.
+
+    On a **POST** request, the form_key specified is checked against the session_key
+    specified.
+
+    :raw-html:`<br />`
+
+    | If they match, the request is allowed to continue.
+    | If no match, the response will be abort(abort_code), default 401.
+
+    :raw-html:`<br />`
+
+    .. code-block::
+
+        @bp.route("/admin", methods=["GET", "POST"])
+        @include_csrf(session_key="csrf", form_key="csrf")
+        def admin_page():
+            ...
+            # You must pass in the CSRF token from the session into the template.
+            # Then add <input type="hidden" name="csrf" value="{{ csrf }}"> to the form.
+            return render_template("admin.html", csrf=session.get("csrf"))
+
+    :raw-html:`<br />`
+
+    -----
+
+    :param session_key: The session key to store the CSRF token in.
+    :param form_key: The form key to check against the session key.
+    :param abort_code: The abort code to use if the CSRF check fails.
+    :return: The decorated function, or abort(abort_code).
     """
+
     def include_csrf_wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
@@ -250,13 +329,13 @@ def include_csrf(session_key: str = "csrf", form_key: str = "csrf"):
                 _form_key = request.form.get(form_key)
 
                 if _form_key is None:
-                    return abort(401)
+                    return abort(abort_code)
 
                 if _session_key is None:
-                    return abort(401)
+                    return abort(abort_code)
 
                 if _session_key != _form_key:
-                    return abort(401)
+                    return abort(abort_code)
 
             return func(*args, **kwargs)
 
