18f616ed24fb455b245dcd2054cd8897ac03b36d
David Carmichael <carmichaelits@gmail.com>
2023-12-01 19:53:58 +0000

    changing build and lint tools to flit and ruff

--- src/flask_imp/__init__.py
+++ src/flask_imp/__init__.py
@@ -3,6 +3,8 @@ from .auth import PasswordGeneration as PasswordGeneration
 from .blueprint import ImpBlueprint as Blueprint
 from .imp import Imp as Imp
 
+__version__ = "2.7.8"
+
 __all__ = [
     "Auth",
     "PasswordGeneration",
