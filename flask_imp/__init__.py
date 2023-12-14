d192f0b253695c77a8b6e57d15cbca940f9a781c
David Carmichael <carmichaelits@gmail.com>
2023-12-14 07:51:41 +0000

    Version update

--- flask_imp/__init__.py
+++ flask_imp/__init__.py
@@ -3,6 +3,6 @@ from .auth import PasswordGeneration as PasswordGeneration
 from .blueprint import ImpBlueprint as Blueprint
 from .imp import Imp as Imp
 
-__version__ = "2.7.9"
+__version__ = "2.7.10"
 
 __all__ = ["Auth", "PasswordGeneration", "Imp", "Blueprint"]
