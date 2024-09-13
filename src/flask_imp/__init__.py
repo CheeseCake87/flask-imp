5a05144a9c0a65b97b6b7a4984775619b97156a9
David Carmichael <carmichaelits@gmail.com>
2024-09-13 08:48:41 +0100

    add cli version check

--- src/flask_imp/__init__.py
+++ src/flask_imp/__init__.py
@@ -1,9 +1,11 @@
+from .__version__ import __version__
 from .auth import Auth as Auth
 from .auth import PasswordGeneration as PasswordGeneration
 from .imp import Imp as Imp
 from .imp_blueprint import ImpBlueprint
 
 __all__ = [
+    "__version__",
     "Auth",
     "PasswordGeneration",
     "Imp",
--- src/flask_imp/_cli/__init__.py
+++ src/flask_imp/_cli/__init__.py
@@ -1,12 +1,14 @@
 import click
 
-from .blueprint import add_blueprint as _add_blueprint
+from .. import __version__
 from .blueprint import add_api_blueprint as _add_api_blueprint
+from .blueprint import add_blueprint as _add_blueprint
 from .helpers import Sprinkles as Sp
 from .init import init_app as _init_app
 
 
 @click.group()
+@click.version_option(__version__)
 def cli() -> None:
     pass  # Entry Point
 
