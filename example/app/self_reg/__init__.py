3715571ae70cdd54c3ec047074857722ae606714
David Carmichael <carmichaelits@gmail.com>
2025-02-04 16:26:55 +0000

    ruff format

--- example/app/self_reg/__init__.py
+++ example/app/self_reg/__init__.py
@@ -1,12 +1,15 @@
 from flask_imp import ImpBlueprint
 from flask_imp.config import ImpBlueprintConfig
 
-bp = ImpBlueprint(__name__, ImpBlueprintConfig(
-    enabled=True,
-    url_prefix="/self_reg",
-    static_folder="static",
-    template_folder="templates",
-    init_session={"self_reg_session_loaded": True},
-))
+bp = ImpBlueprint(
+    __name__,
+    ImpBlueprintConfig(
+        enabled=True,
+        url_prefix="/self_reg",
+        static_folder="static",
+        template_folder="templates",
+        init_session={"self_reg_session_loaded": True},
+    ),
+)
 
 bp.import_resources("routes")
