168c773cd65fd2aeb91d9aaf544016026272cb53
David <carmichaelits@gmail.com>
2025-10-14 22:45:53 +0100

    refactor and add APIKeyCheckpoint

--- src/flask_imp/security/checkpoints/__init__.py
+++ src/flask_imp/security/checkpoints/__init__.py
@@ -1,71 +1,178 @@
+"""
+Basic:
+{
+    "auth.parameters": {
+        "password": "pass",
+        "username": "user"
+    },
+    "auth.token": null,
+    "auth.type": "basic"
+}
+
+Bearer:
+{
+    "auth.parameters": {},
+    "auth.token": "hello",
+    "auth.type": "bearer"
+}
+
+"""
+
 import typing as t
+from functools import partial
+
+from flask import request
+from flask import session
+
+from .._private_funcs import _check_against_values_allowed
 
 
-class AuthorizationBearerCheckpoint:
-    token: str
-    pass_endpoint: t.Optional[str]
-    fail_endpoint: t.Optional[str]
-    endpoint_kwargs: t.Optional[t.Dict[str, t.Union[str, int]]]
+class _Checkpoint:
+    pass_url: t.Optional[t.Union[str | partial]] = None
+    fail_url: t.Optional[t.Union[str | partial]] = None
     fail_json: t.Optional[t.Dict[str, t.Any]]
     fail_status: int
     message: t.Optional[str]
     message_category: str
 
+
+class APIKeyCheckpoint(_Checkpoint):
+    key: str
+    header: str
+    type_: t.Literal["header", "query_param"]
+
     def __init__(
         self,
-        token: str,
-        pass_endpoint: t.Optional[str] = None,
-        fail_endpoint: t.Optional[str] = None,
-        endpoint_kwargs: t.Optional[t.Dict[str, t.Union[str, int]]] = None,
+        key: str,
+        type_: t.Literal["header", "query_param"] = "header",
+        header_or_param: str = "x-api-key",
+        pass_url: t.Optional[t.Union[str | partial]] = None,
+        fail_url: t.Optional[t.Union[str | partial]] = None,
         fail_json: t.Optional[t.Dict[str, t.Any]] = None,
         fail_status: int = 403,
         message: t.Optional[str] = None,
         message_category: str = "message",
     ):
         """
-        A checkpoint that checks if the Authorization header is set to a valid token.
+        A checkpoint that checks if the specified header or query parameter exists,
+        and that the key in the request is valid.
+
+        Note: Query parameters are case-sensitive.
 
         If fail_json is provided, passing to endpoints will be disabled.
 
+        `pass_url` and `fail_url` take either a string or the `lazy_url_for` function.
+
+        Here's an example of `lazy_url_for` usage::
+
+            from flask_imp.utilities import lazy_url_for
+
+            @app.get("/protected")
+            @checkpoint(...(..., pass_url=lazy_url_for(endpoint)))
+            ...
+
+        ---
+
+        :param key: the key to validate against
+        :param type_: where to look for the key
+        :param header_or_param: what header or query param value will the key be expected
+        :param pass_url: the url to redirect to if the key value passes
+        :param fail_url: the url to redirect to if the key value fails
+        :param fail_json: JSON that is returned on failure - defaults to {"error": "You are not logged in."}
+        :param fail_status: the status code to abort with if the session key does not exist or match the pass_value
+        :param message: a message to add to flask.flash()
+        :param message_category: the category of the flash message
+        """
+        self.key = key
+        self.type_ = type_
+        self.header_or_param = header_or_param
+        self.pass_url = pass_url
+        self.fail_url = fail_url
+        self.fail_json = fail_json
+        self.fail_status = fail_status
+        self.message = message
+        self.message_category = message_category
+
+    def pass_(self):
+        if self.type_ == "header":
+            if header_value := request.headers.get(self.header_or_param):
+                if header_value == self.key:
+                    return True
+
+        if self.type_ == "query_param":
+            # Query Params are case-sensitive :(
+            if query_param_value := request.args.get(self.header_or_param):
+                if query_param_value == self.key:
+                    return True
+
+        return False
+
+
+class BearerCheckpoint(_Checkpoint):
+    token: str
+
+    def __init__(
+        self,
+        token: str,
+        pass_url: t.Optional[t.Union[str | partial]] = None,
+        fail_url: t.Optional[t.Union[str | partial]] = None,
+        fail_json: t.Optional[t.Dict[str, t.Any]] = None,
+        fail_status: int = 403,
+        message: t.Optional[str] = None,
+        message_category: str = "message",
+    ):
+        """
+        A checkpoint that checks if the authorization header is of type Bearer,
+        and that the token in the request is valid.
+
+        If fail_json is provided, passing to endpoints will be disabled.
+
+        `pass_url` and `fail_url` take either a string or the `lazy_url_for` function.
+
+        Here's an example of `lazy_url_for` usage::
+
+            from flask_imp.utilities import lazy_url_for
+
+            @app.get("/protected")
+            @checkpoint(...(..., pass_url=lazy_url_for(endpoint)))
+            ...
+
+        ---
+
         :param token: the token to check for
-        :param fail_endpoint: the endpoint to redirect to if the session key does not exist or
-                              match the pass_value
-        :param pass_endpoint: the endpoint to redirect to if the session key passes
-                              Used to redirect away from login pages, if already logged in
-        :param endpoint_kwargs: a dictionary of keyword arguments to pass to the redirect endpoint
+        :param pass_url: the url to redirect to if the token value passes
+        :param fail_url: the url to redirect to if the token value fails
         :param fail_json: JSON that is returned on failure - defaults to {"error": "You are not logged in."}
         :param fail_status: the status code to abort with if the session key does not exist or match the pass_value
         :param message: a message to add to flask.flash()
         :param message_category: the category of the flash message
         """
         self.token = token
-        self.pass_endpoint = pass_endpoint
-        self.fail_endpoint = fail_endpoint
-        self.endpoint_kwargs = endpoint_kwargs
+        self.pass_url = pass_url
+        self.fail_url = fail_url
         self.fail_json = fail_json
         self.fail_status = fail_status
         self.message = message
         self.message_category = message_category
 
+    def pass_(self):
+        if auth := request.authorization:
+            if auth.type == "bearer" and auth.token == self.token:
+                return True
 
-class SessionCheckpoint:
+        return False
+
+
+class SessionCheckpoint(_Checkpoint):
     session_key: str
     values_allowed: t.Union[t.List[t.Union[str, int, bool]], str, int, bool]
-    pass_endpoint: t.Optional[str] = None
-    fail_endpoint: t.Optional[str] = None
-    endpoint_kwargs: t.Optional[t.Dict[str, t.Union[str, int]]] = None
-    fail_json: t.Optional[t.Dict[str, t.Any]] = None
-    fail_status: int = 403
-    message: t.Optional[str] = None
-    message_category: str = "message"
 
     def __init__(
         self,
         session_key: str,
         values_allowed: t.Union[t.List[t.Union[str, int, bool]], str, int, bool],
-        pass_endpoint: t.Optional[str] = None,
-        fail_endpoint: t.Optional[str] = None,
-        endpoint_kwargs: t.Optional[t.Dict[str, t.Union[str, int]]] = None,
+        pass_url: t.Optional[t.Union[str | partial]] = None,
+        fail_url: t.Optional[t.Union[str | partial]] = None,
         fail_json: t.Optional[t.Dict[str, t.Any]] = None,
         fail_status: int = 403,
         message: t.Optional[str] = None,
@@ -76,13 +183,24 @@ class SessionCheckpoint:
 
         If fail_json is provided, passing to endpoints will be disabled.
 
+        `pass_url` and `fail_url` take either a string or the `lazy_url_for` function.
+
+        Here's an example of `lazy_url_for` usage::
+
+            from flask_imp.utilities import lazy_url_for
+
+            @app.get("/protected")
+            @checkpoint(...(..., pass_url=lazy_url_for(endpoint)))
+            ...
+
+        ---
+
         :param session_key: the session key to check for
         :param values_allowed: a list of or singular value(s) that the session key must contain
-        :param fail_endpoint: the endpoint to redirect to if the session key does not exist or
-                              match the pass_value
-        :param pass_endpoint: the endpoint to redirect to if the session key passes
-                              Used to redirect away from login pages, if already logged in
-        :param endpoint_kwargs: a dictionary of keyword arguments to pass to the redirect endpoint
+        :param pass_url: the url to redirect to if the session key passes.
+                          Used to redirect away from login pages, if already logged in
+        :param fail_url: the url to redirect to if the session key does not exist or
+                          match any of the values in values_allowed
         :param fail_json: JSON that is returned on failure - defaults to {"error": "You are not logged in."}
         :param fail_status: the status code to abort with if the session key does not exist or match the pass_value
         :param message: a message to add to flask.flash()
@@ -90,13 +208,19 @@ class SessionCheckpoint:
         """
         self.session_key = session_key
         self.values_allowed = values_allowed
-        self.pass_endpoint = pass_endpoint
-        self.fail_endpoint = fail_endpoint
-        self.endpoint_kwargs = endpoint_kwargs
+        self.pass_url = pass_url
+        self.fail_url = fail_url
         self.fail_json = fail_json
         self.fail_status = fail_status
         self.message = message
         self.message_category = message_category
 
+    def pass_(self) -> bool:
+        if skey := session.get(self.session_key):
+            if _check_against_values_allowed(skey, self.values_allowed):
+                return True
 
-__all__ = ["AuthorizationBearerCheckpoint", "SessionCheckpoint"]
+        return False
+
+
+__all__ = ["APIKeyCheckpoint", "BearerCheckpoint", "SessionCheckpoint"]
