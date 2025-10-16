0ba6a0f47387ea7d219b8ce0386503e980e068ec
David <carmichaelits@gmail.com>
2025-10-16 21:16:49 +0100

    update docstrings

--- src/flask_imp/security/_checkpoint.py
+++ src/flask_imp/security/_checkpoint.py
@@ -28,63 +28,64 @@ def checkpoint(checkpoint_: AnyCheckpoint) -> t.Callable[..., t.Any]:
 
     Example of a route that requires a user to be logged in::
 
-        @bp.route("/admin", methods=["GET"])
-        @checkpoint(
-            SessionCheckpoint(
-                'logged_in',
-                True,
-                fail_url=lazy_url_for('blueprint.login_page'),
-                message="Login needed"
-            )
+        LOGIN_REQUIRED = SessionCheckpoint(
+            'logged_in', True
+        ).action(
+            lazy_url_for('blueprint.login_page')
         )
+
+        @bp.route("/admin", methods=["GET"])
+        @checkpoint(LOGIN_REQUIRED)
         def admin_page():
             ...
 
     Example of a route that if the user is already logged in, redirects to the specified endpoint::
 
-        @bp.route("/login-page", methods=["GET"])
-        @checkpoint(
-            SessionCheckpoint(
-                'logged_in',
-                True,
-                pass_url=lazy_url_for('blueprint.admin_page'),
-                message="Already logged in"
-            )
+        IS_LOGGED_IN = SessionCheckpoint(
+            'logged_in', True
+        ).action(
+            pass_url=lazy_url_for('blueprint.admin_page'),
+            message="Already logged in"
         )
+
+        @bp.route("/login-page", methods=["GET"])
+        @checkpoint(IS_LOGGED_IN)
         def login_page():
             ...
 
     Example of a route that requires a user to be logged in and to be an admin::
 
+        LOGIN_REQUIRED = SessionCheckpoint(
+            session_key="logged_in",
+            values_allowed=True,
+        ).action(
+            fail_url=lazy_url_for("blueprint.login_page"),
+        )
+
+        PERM_REQUIRED = SessionCheckpoint(
+            session_key="user_type",
+            values_allowed="admin",
+        ).action(
+            fail_url=lazy_url_for("blueprint.index"),
+            message="You need to be an admin to access this page"
+        )
+
         @bp.route("/admin", methods=["GET"])
-        @checkpoint(
-            SessionCheckpoint(
-                'logged_in',
-                True,
-                fail_url=lazy_url_for('blueprint.login_page'),
-                message="Login needed"
-            )
-        )
-        @checkpoint(
-            SessionCheckpoint(
-                'user_type',
-                'admin',
-                fail_url=lazy_url_for('blueprint.index'),
-                message="You need to be an admin to access this page"
-            )
-        )
+        @checkpoint(LOGIN_REQUIRED)
+        @checkpoint(PERM_REQUIRED)
         def admin_page():
             ...
 
     You can also supply your own failed return JSON::
 
+        JS_FETCH_CHECKPOINT = SessionCheckpoint(
+            'logged_in', True
+        ).action(
+            fail_json={"error": "You are not logged in."}
+        )
+
         @bp.route("/api/resource", methods=["GET"])
-        @checkpoint(
-            SessionCheckpoint(
-                'logged_in',
-                True,
-                fail_json={"error": "You are not logged in."}
-            )
+        @checkpoint(JS_FETCH_CHECKPOINT)
         def api_page():
             ...
 
@@ -95,7 +96,7 @@ def checkpoint(checkpoint_: AnyCheckpoint) -> t.Callable[..., t.Any]:
         fetch("/api/resource", {
             method: "GET",
             credentials: "include"
-        })
+        })...
 
     :param checkpoint_: the checkpoint class to pass or fail
     :return: the decorated function, or abort(fail_status), or redirect, or fail_json response
